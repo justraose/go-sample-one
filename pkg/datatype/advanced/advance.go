6d2f054cf1ab8b8dc8207830a57f5e468a83650e
justraose <542353778@qq.com>
2018-11-20 00:42:01 +0800

    1

--- pkg/datatype/advanced/advance.go
+++ pkg/datatype/advanced/advance.go
@@ -36,6 +36,10 @@ func TestArray() {
 	numbers2[4] = len(numbers2)
 	sum := 11
 
+	for i, data := range numbers2 {
+		fmt.Println(i, " ", data)
+	}
+
 	// “==”用于两个值的相等性判断
 	fmt.Printf("%v\n", (sum == numbers2[0]+numbers2[1]+numbers2[2]+numbers2[3]+numbers2[4]))
 }
